00ad3be446a3df20d7fe23e45fb626fbd3cf33b9
Md Razik Khan <mdrazikkhan779@gmail.com>
2025-01-15 17:40:00 +0530

    add new files

--- codeFile/Ep2/app.js
+++ codeFile/Ep2/app.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 Tag"),
+    React.createElement("h1", {}, "hey!! "),
     React.createElement("h2", {}, "I'm h2 Tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
